da00cba69e9734ff4b7898b42be061c2d9e9487c
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-08-01 07:14:05 -0700

    Remove p2p stats class

--- lib/screens/demo_screen.dart
+++ lib/screens/demo_screen.dart
@@ -75,17 +75,3 @@ class _VidstackPlayerScreenState extends State<VidstackPlayerScreen> {
     );
   }
 }
-
-class P2PStats {
-  double totalHttpDownloaded;
-  double totalP2PDownloaded;
-  double totalP2PUploaded;
-  Set<String> activePeers;
-
-  P2PStats({
-    this.totalHttpDownloaded = 0,
-    this.totalP2PDownloaded = 0,
-    this.totalP2PUploaded = 0,
-    this.activePeers = const {},
-  });
-}
